db812da5b3094419e2b80862988239e77b06d541
Galaxkk <dito.o.peixoto09@gmail.com>
2024-09-06 09:04:46 -0300

    atualização tabuada

--- Classicos/src/ex_Tabuada.java
+++ Classicos/src/ex_Tabuada.java
@@ -2,14 +2,19 @@
 public class ex_Tabuada {
 	public static void main(String[] args) {
         int n1 = 1, i = 1;
-        while (n1 <= 10) {
-            System.out.println("Tabuada do " + n1 + ":");
-            while (i <= 10) {
+        while (n1 < 10) {
+            while (i < 11) {
                 System.out.println(n1 + " x " + i + " = " + (n1 * i));
                 i++;
             }
             System.out.println(); 
             n1++;
+            i = 2;
+            System.out.println("Tabuada do " + n1 + ":");
+            while (i < 11){
+                System.out.println(n1 + " x " + i + " = " + (n1 * i));
+                i++;
+            }
         }
     }
 }
