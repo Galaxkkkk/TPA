62294e626f6f905c28286af781b6f4a926674cdf
Galaxkk <dito.o.peixoto09@gmail.com>
2024-09-12 20:12:04 -0300

    exercicios novos Java

---
+++ Exercicios-Laço-Classico/fibonacci.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class fibonacci {
+    public static void main(String[] args) {
+            int n, n2=0, n3=1, f=0, i=0;
+            Scanner input = new Scanner(System.in);
+            System.out.print("Digite um Número: ");
+            n = input.nextInt();
+            while (i<n){
+            System.out.println(f+"");
+            f = n2+n3;
+            n2=n3;
+            n3=f;
+            i++;
+        }
+    }
+}
