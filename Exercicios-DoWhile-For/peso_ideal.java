27dff47df5d4d16c73a6c1a8cc7b16c5a76b24a6
Galaxkk <dito.o.peixoto09@gmail.com>
2024-09-19 20:55:05 -0300

    aaaaaaaaa

--- Exercicios-DoWhile-For/peso_ideal.java
+++ Exercicios-DoWhile-For/peso_ideal.java
@@ -1,17 +1,12 @@
 import java.util.*;
 public class peso_ideal {
   public static void main(String[] args) {
-    // Declarando Váriaveis
     String sexo, resposta;
     double altura, pesoIdeal;
 
-    // Inicializando Scanner
     Scanner input = new Scanner(System.in);
 
-    // Obtendo Peso Ideal do Amiguinho
     do{
-      
-    //Obtendo Genero e Altura do Usuário
     System.out.println("Digite o seu Genero (M para Masculino ou F para Feminino): ");
     sexo = input.next();
     System.out.println("Digite a sua altura (em metros): ");
@@ -24,7 +19,7 @@ public class peso_ideal {
         pesoIdeal = (52 + (0.67 * ((altura * 100) - 152.4)));
         System.out.printf("O seu peso é %.2f\n", pesoIdeal);
       }
-      // Perguntando se quer continuar
+
       System.out.println("Deseja continuar? (S para Sim N para Não): ");
       resposta = input.next();
     }while (resposta.equalsIgnoreCase("S"));
